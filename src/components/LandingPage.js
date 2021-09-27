ff2494b7328088c719d34db35358728b7650bb74
fredrikasundell <70134993+FredrikaSundell@users.noreply.github.com>
2021-09-27 17:16:44 +0200

    first commit

---
+++ src/components/LandingPage.js
@@ -0,0 +1,11 @@
+const LandingPage = () => {
+    return (
+      <div className='landing'>
+        <h1>Hey from LandingPage</h1>
+        <h2>blablabla</h2>
+        <button className='btn'>Go</button>
+      </div>
+    )
+}
+
+export default LandingPage
